c794f178ed5aa04be77278899696b367964ad5eb
Helga <levchenkohelga@gmail.com>
2023-08-27 12:47:53 +0300

    HW3

---
+++ ht3script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/* task 1 */
+let gettedNumber = Number(prompt("Enter the integer number", ""));
+if (isNaN(gettedNumber)) {
+    console.log("Помилка: введіть число");
+}
+else if (!isNaN(gettedNumber)) {
+    for( let i  = 0; i < gettedNumber; i += 2 ) {
+        console.log(i);
+    }
+} //
+
+/* task 2 */
+const currentMaxValue = 4589;
+let reverseMaxValue;
+reverseMaxValue = Number(currentMaxValue.toString().split("").reverse().join(""));
+console.log(reverseMaxValue); 
+console.log(typeof reverseMaxValue); //
+
+/* task 3 */
+const resultsArray = [1, 2, [3, [4]]];
+let productOfArray = resultsArray.flat(Infinity); // [1, 2, 3, 4]
+let result = 1;
+for (let i=0; i<productOfArray.length; i++){
+    result *= productOfArray[i];
+}
+console.log(result); // 24
