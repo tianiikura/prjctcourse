847caf190426a8359d5dc8c6f12fc5dd67e98b37
Helga <levchenkohelga@gmail.com>
2023-09-14 18:28:40 +0300

    fixed

--- ht5script.js
+++ ht5script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //task 1 
-/ * function iterativeOddSumTo(number) {
+/* function iterativeOddSumTo(number) {
     let sum = 0;
     for( let i  = 1; i <= number; i += 2 ) {
         sum += i;
@@ -16,16 +16,12 @@
 
 //task 2
 function recursiveOddSumTo(number) {
-    if (number <= 0) {
-        return 0;
-    }
-    
-    else if (number % 2 === 0) {
-        number --;
-    }
-    
     if (number === 1) {
         return number;
+    }
+    
+    if (number % 2 === 0) {
+        return recursiveOddSumTo(number - 1);
     }
 
     return number + recursiveOddSumTo(number - 2);
