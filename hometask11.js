0e18e8345d81b8438861860742d369735c8a7453
Helga <levchenkohelga@gmail.com>
2023-10-08 16:31:25 +0300

    ht11

---
+++ hometask11.js
@@ -0,0 +1,7 @@
+"use scrict"
+let header2 = document.getElementById("headerTwo");
+let firstSection = document.getElementsByClassName("firstSection");
+// let firstSection = document.querySelector("section.firstSection");
+let listItem5 = document.getElementsByTagName("ul:nth-child(5)");
+let hatredLevelLabel = document.querySelector("label.hatredLevelLabel");
+let hatredLevelBlock = hatredLevelLabel.closest(".hatredLevelLabel");
