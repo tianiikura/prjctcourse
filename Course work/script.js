5e11d79401d71fa06bae141750c87c01831c33dd
Helga <levchenkohelga@gmail.com>
2023-10-29 23:40:38 +0200

    course-prj

--- Course work/script.js
+++ Course work/script.js
@@ -1,4 +1,26 @@
 "use strict";
+//Перехід між табами
+function initTabs () {
+    let tabSwitchButton1 = document.querySelector(".tab1");
+    let tabSwitchButton2 = document.querySelector(".tab2");
+    let firstTab = document.querySelector(".first-tab");
+    let secondTab = document.querySelector(".second-tab");
+    let firstTabClasses = firstTab.classList;
+    let secondTabClasses = secondTab.classList;
+
+    function switchBetweenTabs (event) {
+        if (event.target === tabSwitchButton2) {
+            secondTabClasses.remove("disabled");
+            firstTabClasses.add("disabled");
+        }
+        else if (event.target === tabSwitchButton1) {
+            firstTabClasses.remove("disabled");
+            secondTabClasses.add("disabled");
+        }
+    };
+    tabSwitchButton1.addEventListener("click", switchBetweenTabs);
+    tabSwitchButton2.addEventListener("click", switchBetweenTabs);
+};
 //розрахунок періоду між датами
 function durationBetweenDates (startDate, endDate, dimension, daysOption) {
     let startDateTime = new Date(startDate);
@@ -60,7 +82,6 @@ function runDateCalculting () {
     const weekPeriodButton = document.getElementById("weekPeriod");
     const monthPeriodButton = document.getElementById("monthPeriod");
 
-    datesHistory.forEach(addToTable);
 
     //Виставленн дати не раніше початкової і не пізніше кінцевої
     const enableEndDateInput = function () {
@@ -85,7 +106,6 @@ function runDateCalculting () {
     }
     weekPeriodButton.addEventListener("click", chooseDatePeriod);
     monthPeriodButton.addEventListener("click", chooseDatePeriod);
-
 
     const calculateTime = function (event) {
         event.preventDefault();
@@ -114,7 +134,6 @@ function runDateCalculting () {
     dateForm.addEventListener("submit", calculateTime);
 }
 
-document.addEventListener("DOMContentLoaded", runDateCalculting);
 
 let datesHistory = JSON.parse(localStorage.getItem("datesHistory")) || []; 
 
@@ -143,4 +162,120 @@ function addToTable (record) {
 
 
     tableBody.children.length > 10 && tableBody.firstElementChild.remove();
+};
+
+function runApp () {
+    datesHistory.forEach(addToTable);
+    runDateCalculting();
+    initTabs();
+    initHolidayTab();
+}
+document.addEventListener("DOMContentLoaded", runApp);
+
+const API_URL = "https://calendarific.com/api/v2/";
+const API_KEY = "pSAlqM4GMzkfE7RyfpUukFAaFhgjLFHJ";
+async function loadCountries() {
+    const response = await fetch(API_URL + "countries" + "?api_key=" + API_KEY);
+    const countries = await response.json();
+    console.log(countries);
+    return countries.response.countries;
+}
+async function loadHolidays(country, year) {
+    const response = await fetch(
+        API_URL+"holidays" + "?api_key=" + API_KEY +
+        "&country=" + country + "&year=" + year
+    );
+    const holidays = await response.json();
+    console.log(holidays);
+    return holidays.response.holidays;
+}
+
+function initHolidayTab () {
+    const countriesList = document.getElementById("countriesList");
+    const yearsList = document.getElementById("yearsList");
+    let holidaysTable = document.querySelector(".holiday-list_table");
+    let tableBody = document.querySelector(".holiday-list_table-body");
+    let dateSort = document.querySelector(".holiday-list_date-sort");
+    let isSortingDown = false;
+   
+    async function createCountryList() {
+        let countries = await loadCountries();  
+        countries.forEach((country) => {
+            let countryOption = document.createElement("option");
+            countryOption.textContent = country.country_name;
+            countryOption.value = country["iso-3166"];
+            countriesList.append(countryOption);
+        })
+    }
+
+    function createYearList() {
+        const currentYear = (new Date).getFullYear();
+        for (let year = 2001; year < 2050; year ++) {
+            let yearOption = document.createElement("option");
+            yearOption.textContent = year;
+            yearOption.value = year;
+            yearsList.append(yearOption);
+        }
+        yearsList.value = currentYear;
+    }
+    
+    async function renderHolidayList() {
+        let selectedCountry = countriesList.value; 
+        let selectedYear = yearsList.value;
+        if(!selectedCountry || !selectedYear) {
+            return;
+        }
+        while(tableBody.firstElementChild) {
+            tableBody.firstElementChild.remove();
+         }
+        let holidays= await loadHolidays(selectedCountry, selectedYear);
+        holidays = holidays.map((holiday) => ({
+            name: holiday.name,
+            iso: holiday.date.iso.slice(0, 10),
+        }))
+
+        holidays.sort(sortByProperty("iso", isSortingDown));
+        console.log(holidays);
+        holidays.forEach((holiday) => {
+            let newRow = document.createElement("tr");
+            tableBody.append(newRow);
+        
+            let cell = document.createElement("td");
+            newRow.append(cell);
+            cell.innerText = holiday.iso;
+        
+            cell = document.createElement("td");
+            newRow.append(cell);
+            cell.innerText = holiday.name;
+        });
+        holidaysTable.classList.remove("disabled"); 
+    };
+
+    function toggleDateSort () {
+        isSortingDown = !isSortingDown;
+        dateSort.classList.toggle("sorting-down", isSortingDown);
+
+        renderHolidayList();
+    }
+
+    createYearList();
+    createCountryList();
+    renderHolidayList();
+    toggleDateSort();
+
+    countriesList.addEventListener("change", renderHolidayList);
+    yearsList.addEventListener("change", renderHolidayList);
+    dateSort.addEventListener("click", toggleDateSort);
+}
+
+function sortByProperty(property, direction) {
+    if (direction) {
+        return (a, b) => {
+            return a[property] > b[property] ? 1 : -1;
+          };
+    } else {
+        return (a, b) => {
+            return a[property] < b[property] ? 1 : -1;
+          };
+    }
 }
