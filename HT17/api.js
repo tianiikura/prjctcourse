a65bb4777dbf9ceb618318237059d3dd202cd1d5
Helga <levchenkohelga@gmail.com>
2023-11-12 23:34:09 +0200

    git api

---
+++ HT17/api.js
@@ -0,0 +1,63 @@
+class API {
+    apiUrl = "https://api.github.com";
+    #token = "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";
+  
+    async getUser(userName) {
+      const response = await fetch(`${this.apiUrl}/users/${userName}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${this.#token}`
+        }
+      });
+      const data = await response.json();
+  
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+  
+      return data;
+    }
+  
+    async getFollowers(userName, amount = 10) {
+      const response = await fetch(
+        `${this.apiUrl}/users/${userName}/followers?per_page=${amount}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${this.#token}`
+          }
+        }
+      );
+      const data = await response.json();
+  
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+  
+      return data;
+    }
+
+    async getRepoList(userName, amount = 5, sort = "pushed") {
+        const response = await fetch(
+            `${this.apiUrl}/users/${userName}/repos?per_page=${amount}&sort=${sort}`,
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${this.#token}`,
+                    Accept: "application/vnd.github+json",
+                    "X-GitHub-Api-Version": "2022-11-28"
+                }
+            }
+        );
+        const data = await response.json();
+    
+        if (!response.ok) {
+        throw new Error(data.message);
+        }
+      
+        return data;
+    }
+}
+  
+  export const api = new API();
+  
